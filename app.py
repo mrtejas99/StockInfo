a5b31cb60c91b723043805af8cb442526f3e93b4
Tejas Lotlikar <mrt3j45@gmail.com>
2021-06-03 00:17:45 +0530

    caching implemented using request_cache

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
-from datetime import date
+from datetime import date, datetime
 from dateutil.relativedelta import  relativedelta
 import requests
 from flask import Flask, render_template
+import requests_cache
+
 app = Flask(__name__)
-from time import sleep    
-    
-@app.route('/favicon.png')
+requests_cache.install_cache('listing_cache', backend='sqlite', expire_after=180)
+
+@app.route('/favicon.png')  
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png', mimetype='image/vnd.microsoft.icon')
-      
 
-@app.route("/")
+@app.route("/")    
 def index():
     return render_template("loading.html")
+
 
 @app.route("/concalls")
 def concalls():
     current = date.today()
-    previous = current + relativedelta(months=-1) #go 4 months back
+    previous = current + relativedelta(months=-4) #go 4 months back
 
     print("previous date is {} and current date is {}".format(previous, current)) 
 
-    #Create session and add UA for visiting the site
-    session = requests.Session()
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
 
     url = "https://api.bseindia.com/BseIndiaAPI/api/AnnGetData/w?strCat=Company+Update&strPrevDate={0}&strScrip=&strSearch=P&strToDate={1}&strType=C".format(previous.strftime("%Y%m%d"), current.strftime("%Y%m%d"))
-    #sleep(10)
-    res=session.get(url, headers=headers).json()
+    data=requests.get(url, headers=headers)
+    print("Current Time: {0} / Used Cache: {1}".format(datetime.now().strftime("%H:%M:%S"), data.from_cache))
+    res=data.json()
     return render_template("concalls.html", records=res['Table'])
 
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', debug=True)
+    app.run(debug = True)
