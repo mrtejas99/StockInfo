e04564039f8adb89e94bd8121d3e61d02ff33182
Tejas Lotlikar <mrt3j45@gmail.com>
2021-06-03 13:23:36 +0530

    Added footer, PPT links

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import date, datetime
 from dateutil.relativedelta import  relativedelta
 import requests
@@ -27,6 +28,8 @@ def concalls():
 
     url = "https://api.bseindia.com/BseIndiaAPI/api/AnnGetData/w?strCat=Company+Update&strPrevDate={0}&strScrip=&strSearch=P&strToDate={1}&strType=C".format(previous.strftime("%Y%m%d"), current.strftime("%Y%m%d"))
     data=requests.get(url, headers=headers)
+    print("retrived {} MB".format(sys.getsizeof(data) / 1048576))
     print("Current Time: {0} / Used Cache: {1}".format(datetime.now().strftime("%H:%M:%S"), data.from_cache))
     res=data.json()
     return render_template("concalls.html", records=res['Table'])
+
